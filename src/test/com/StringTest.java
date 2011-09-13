c67d982dff54028210f8311bc610e11f62f2f00e
Fook Chee Leong <cheeleong_83@yahoo.com>
2011-09-13 19:12:38 +0800

    added 1 line

--- src/test/com/StringTest.java
+++ src/test/com/StringTest.java
@@ -3,5 +3,6 @@ package test.com;
 public class StringTest {
     public static void main(String... args) {
         System.out.println("hello world");
+		System.out.println("hello world2");
     }
 }
