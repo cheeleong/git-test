297b49f94ed426475c391c7bac83149a13a003fd
Fook Chee Leong <cheeleong_83@yahoo.com>
2011-09-19 13:50:47 +0800

    testing

--- src/test/com/StringTest.java
+++ src/test/com/StringTest.java
@@ -4,5 +4,6 @@ public class StringTest {
     public static void main(String... args) {
         System.out.println("hello world");
 		System.out.println("hello world2");
+		System.out.println("hello world3");
     }
 }
